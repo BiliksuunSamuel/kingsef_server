62b96dd89329569d9b3d6d9fe695c384f7284be2
BiliksuunSamuel <biliksuunsamuel@gmail.com>
2022-03-26 12:42:05 +0000

    make it clear

--- build/constants/Routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRoutes = void 0;
-exports.userRoutes = {
-    login: "/api/user/login",
-    register: "/api/user/register",
-};
--- build/messaging/Connections.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateConnectedUsers = void 0;
-const ConnectedUsers = [];
-function UpdateConnectedUsers(users) { }
-exports.UpdateConnectedUsers = UpdateConnectedUsers;
-exports.default = ConnectedUsers;
